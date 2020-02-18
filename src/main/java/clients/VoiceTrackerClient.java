6c38c6d7b4d15960ae8f6559727cd73f8d3eb201
Patrick Ubelhor <pubelhor@gmail.com>
2020-02-18 13:11:13 -0500

    Changed HTTP client to OkHttp

--- src/main/java/clients/VoiceTrackerClient.java
+++ src/main/java/clients/VoiceTrackerClient.java
@@ -1,41 +1,84 @@
 package clients;
 
 import main.Globals;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.jetbrains.annotations.NotNull;
 
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.io.IOException;
 
 /**
  * @author Patrick Ubelhor
- * @version 12/17/2019
+ * @version 2/18/2020
  */
 public class VoiceTrackerClient {
 	
-	private final HttpClient client;
+	private final OkHttpClient client;
+	private final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 	
 	public VoiceTrackerClient() {
-		this.client = HttpClient.newHttpClient();
+		client = new OkHttpClient();
 	}
 	
 	
 	public void logJoinEvent(Long snowflake) {
-		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create(Globals.VOICE_TRACKER_BASE_URL + "/join/" + snowflake))
-				.POST(HttpRequest.BodyPublishers.noBody())
+		
+		Request request = new Request.Builder()
+				.url(Globals.VOICE_TRACKER_BASE_URL + "/join/" + snowflake)
+				.post(RequestBody.create("", JSON))
 				.build();
 		
-		client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+		// Send JOIN request to VoiceTracker asynchronously
+		client.newCall(request).enqueue(new Callback() {
+			@Override
+			public void onFailure(@NotNull Call call, @NotNull IOException e) {
+				Globals.logger.error("Failed to send JOIN request", e);
+			}
+			
+			@Override
+			public void onResponse(@NotNull Call call, @NotNull Response response) {
+				if (!response.isSuccessful()) {
+					Globals.logger.error("Error on JOIN request: " + response.code() + "\n" + response.body());
+					return;
+				}
+				
+				response.close();
+				Globals.logger.debug("Successfully sent JOIN request");
+			}
+		});
+				
+	
 	}
 	
 	public void logLeaveEvent(Long snowflake) {
-		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create(Globals.VOICE_TRACKER_BASE_URL + "/leave/" + snowflake))
-				.POST(HttpRequest.BodyPublishers.noBody())
+		
+		Request request = new Request.Builder()
+				.url(Globals.VOICE_TRACKER_BASE_URL + "/leave/" + snowflake)
+				.post(RequestBody.create("", JSON))
 				.build();
 		
-		client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+		client.newCall(request).enqueue(new Callback() {
+			@Override
+			public void onFailure(@NotNull Call call, @NotNull IOException e) {
+				Globals.logger.error("Failed to send LEAVE request", e);
+			}
+			
+			@Override
+			public void onResponse(@NotNull Call call, @NotNull Response response) {
+				if (!response.isSuccessful()) {
+					Globals.logger.error("Error on LEAVE request: " + response.code() + "\n" + response.body());
+					return;
+				}
+				
+				response.close();
+				Globals.logger.debug("Successfully sent LEAVE request");
+			}
+		});
 	}
 	
 }
