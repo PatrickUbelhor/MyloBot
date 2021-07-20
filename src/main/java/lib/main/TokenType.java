3a8bbcd6aeb9c31009a425044a6d03311de621ed
Patrick Ubelhor <pubelhor@gmail.com>
2021-07-20 07:56:42 -0500

    Improved lexing

--- src/main/java/lib/main/TokenType.java
+++ src/main/java/lib/main/TokenType.java
@@ -2,14 +2,14 @@ package lib.main;
 
 /**
  * @author Patrick Ubelhor
- * @version 2/24/2021
+ * @version 7/19/2021
  */
 public enum TokenType {
 	
 	WHITESPACE("\\s"),
 	COMMAND("^![a-zA-Z]+"),
 	LINK("https?://.+"),
-	WORD("[a-zA-Z]+[\\w\\.]*"),
+	WORD("[a-zA-Z]+[\\w\\.':;/?!]*"),
 	DICEROLL("[0-9]+d[0-9]+"),
 	NUMBER("-?[0-9]+"),
 	QUOTE("\".*\""),
