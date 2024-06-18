943629878e9af7dd4ba031eaafa476128de72982
Patrick Ubelhor <pubelhor@gmail.com>
2024-06-18 01:38:06 -0400

    Added new YouTube audio manager

--- src/main/java/lib/music/Music.java
+++ src/main/java/lib/music/Music.java
@@ -4,9 +4,10 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import dev.lavalink.youtube.YoutubeAudioSourceManager;
 import lib.commands.Command;
-import main.Config;
 import lib.main.Permission;
+import main.Config;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
@@ -22,7 +23,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author Patrick Ubelhor
- * @version 12/3/2023
+ * @version 6/18/2024
  */
 public abstract class Music extends Command {
 	
@@ -35,27 +36,34 @@ public abstract class Music extends Command {
 	protected Music(String name, Permission perm) {
 		super(name, perm);
 	}
-	
-	
+
+
 	@Override
 	protected boolean subInit() {
 		if (!hasInit.getAndSet(true)) { // Each music command (play, skip, etc.) will call this. Only want to run it once.
+			YoutubeAudioSourceManager youtubeSourceManager = new YoutubeAudioSourceManager();
+
+			playerManager.registerSourceManager(youtubeSourceManager);
 			playerManager.enableGcMonitoring();
 			playerManager.setFrameBufferDuration(10000); // 10 seconds
 			playerManager.setPlayerCleanupThreshold(30000); // 30 seconds
-			AudioSourceManagers.registerRemoteSources(playerManager);
+
+			AudioSourceManagers.registerRemoteSources(
+				playerManager,
+				com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager.class
+			);
 		}
 		return true;
 	}
-	
-	
+
+
 	@Override
 	protected final boolean subEnd() {
 		playerManager.shutdown();
 		return true;
 	}
-	
-	
+
+
 	// TODO: Should return track scheduler. On failure, should throw exception.
 	protected final boolean joinAudioChannel(Guild guild, Member member, MessageChannel logChannel) {
 		AudioManager guildAudioManager = guild.getAudioManager();
@@ -70,56 +78,56 @@ public abstract class Music extends Command {
 		if (guildAudioManager.isConnected() && guildAudioManager.getConnectedChannel().getIdLong() == vc.getIdLong()) {
 			return true;
 		}
-		
+
 		Long guildId = guild.getIdLong();
 		trackSchedulers.putIfAbsent(guildId, generatePlayer());
 		TrackScheduler trackScheduler = trackSchedulers.get(guildId);
 		trackScheduler.setLogChannel(logChannel);
-		
+
 		guildAudioManager.setSendingHandler(new AudioPlayerSendHandler(trackScheduler.getPlayer()));
 		guildAudioManager.openAudioConnection(vc);
 		audioManagers.put(guildId, guildAudioManager);
 		return true;
 	}
-	
-	
+
+
 	protected final boolean leaveAudioChannel(MessageReceivedEvent event) {
 		Long guildId = event.getGuild().getIdLong();
-		
+
 		if (!audioManagers.containsKey(guildId)) {
 			event.getChannel().sendMessage("I can't leave a server I'm not in!").queue();
 			return false;
 		}
-		
+
 		AudioManager audioManager = audioManagers.get(guildId);
 		audioManager.closeAudioConnection();
 		audioManagers.remove(guildId);
 		return true;
 	}
-	
-	
+
+
 	protected final boolean leaveAudioChannel(SlashCommandInteractionEvent event) {
 		Long guildId = event.getGuild().getIdLong();
-		
+
 		if (!audioManagers.containsKey(guildId)) {
 			event.reply("I can't leave a server I'm not in!").queue();
 			return false;
 		}
-		
+
 		AudioManager audioManager = audioManagers.get(guildId);
 		audioManager.closeAudioConnection();
 		audioManagers.remove(guildId);
 		return true;
 	}
-	
-	
+
+
 	private TrackScheduler generatePlayer() {
 		AudioPlayer player = playerManager.createPlayer();
 		TrackScheduler trackScheduler = new TrackScheduler(player);
 		player.setVolume(Config.getConfig().MUSIC_VOLUME());
 		player.addListener(trackScheduler); // TODO: How is this not a circular reference?
-		
+
 		return trackScheduler;
 	}
-	
+
 }
